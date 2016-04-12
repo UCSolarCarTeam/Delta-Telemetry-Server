7015ae7f8bb85f8c9c9134378b81a1e36cf1ed48
Enoch Tsang <etsang1@hotmail.com>
2016-04-12 17:24:18 -0600

    cleaned up of code"

--- TelemetryServer.go
+++ TelemetryServer.go
@@ -1,97 +1,73 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	//"github.com/pborman/getopt"
-	//"gopkg.in/ini.v1"
-	"os"
-	//"os/exec"
-	//"path/filepath"
-	// "strconv"
-	//"strings"
-	"time"
+    "fmt"
+    "net"
+    "os"
+    "time"
 )
 
 var KeepAliveMap map[string]KeepAlive
 
 const TimeOut = 105
-const HeartBeatPacket = "KeepMeAlive!"
 
 type KeepAlive struct {
-	address   *net.UDPAddr
-	timestamp time.Time
+    address   *net.UDPAddr
+    timestamp time.Time
 }
 
 func CheckError(err error) {
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(0)
-	}
+    if err != nil {
+        fmt.Println("Error: ", err)
+        os.Exit(0)
+    }
 }
 
 /* cleans out any entries that have been in the Map for longer than the timeout */
 func CleanMap() {
-	for _, contents := range KeepAliveMap {
-		fmt.Println(contents.timestamp)
-		if time.Since(contents.timestamp).Seconds() > TimeOut {
-			fmt.Println(contents.address, "timed out")
-			delete(KeepAliveMap, contents.address.String())
-		}
-	}
+    for _, contents := range KeepAliveMap {
+        fmt.Println(contents.timestamp)
+        if time.Since(contents.timestamp).Seconds() > TimeOut {
+            fmt.Println(contents.address, "timed out")
+            delete(KeepAliveMap, contents.address.String())
+        }
+    }
 }
 
 func UpdateMap(addr *net.UDPAddr) {
-	time_receieved := time.Now()
-	temp := KeepAlive{addr, time_receieved}
-	KeepAliveMap[addr.String()] = temp
-	//fmt.Println(KeepAliveMap)
-	CleanMap()
+    time_receieved := time.Now()
+    temp := KeepAlive{addr, time_receieved}
+    KeepAliveMap[addr.String()] = temp
+    CleanMap()
 }
 func forwardMessagesUDP(ServerConn *net.UDPConn, message []byte, length int) {
-	for _, clients := range KeepAliveMap {
-		fmt.Println("Sending to client ", clients.address.String())
-
-		_, err := ServerConn.WriteToUDP([]byte("This is from the server"), clients.address)		
-		if err != nil {
-    	    fmt.Printf("Couldn't send response %v", err)
-	    }
-
-		// port, err := strconv.Atoi(os.Args[1])
-		// port += port + 0
-		// CheckError(err)
-		// ServerAddrr, err := net.ResolveUDPAddr("udp", ":"+string(port))
-		// Conn, err := net.DialUDP("udp", ServerAddrr, clients.address)
-		// CheckError(err)
-		// defer Conn.Close()
-		// fmt.Println("Sending message to", clients.address.String())
-		// message = []byte("hi\n")
-		// Conn.Write(message)
-		CheckError(err)
-	}
+    for _, clients := range KeepAliveMap {
+        fmt.Println("Sending to client ", clients.address.String())
+        _, err := ServerConn.WriteToUDP([]byte("This is from the server"), clients.address)
+        CheckError(err)
+    }
 }
 
 func ReceiveAndPrintUDP(ServerConn *net.UDPConn, buf []byte) (int, error) {
-	n, addr, err := ServerConn.ReadFromUDP(buf)
-	//fmt.Println("Received ", string(buf[0:n]), " from ", addr)
-	UpdateMap(addr)
-	return n, err
+    n, addr, err := ServerConn.ReadFromUDP(buf)
+    fmt.Println("Received ", string(buf[0:n]), " from ", addr)
+    UpdateMap(addr)
+    return n, err
 }
 
 func main() {
-	KeepAliveMap = make(map[string]KeepAlive)
-	ServerAddr, err := net.ResolveUDPAddr("udp", ":"+os.Args[1])
-	CheckError(err)
-	ServerConn, err := net.ListenUDP("udp", ServerAddr)
-	CheckError(err)
-	defer ServerConn.Close()
+    KeepAliveMap = make(map[string]KeepAlive)
+    ServerAddr, err := net.ResolveUDPAddr("udp", ":"+os.Args[1])
+    CheckError(err)
+    ServerConn, err := net.ListenUDP("udp", ServerAddr)
+    CheckError(err)
+    defer ServerConn.Close()
 
-	buf := make([]byte, 2048)
+    buf := make([]byte, 2048)
 
-	for {
-		n, err := ReceiveAndPrintUDP(ServerConn, buf)
-		fmt.Println("Received ", string(buf[0:n]))
-		go forwardMessagesUDP(ServerConn, buf, n)
-		CheckError(err)
-	}
+    for {
+        n, err := ReceiveAndPrintUDP(ServerConn, buf)
+        go forwardMessagesUDP(ServerConn, buf, n)
+        CheckError(err)
+    }
 }
--- testclient.go
+++ testclient.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
-	"strconv"
-	"time"
-	"bufio"
+    "fmt"
+    "net"
+    "os"
+    "strconv"
+    "time"
+    "bufio"
 )
 
 func CheckError(err error) {
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
+    if err != nil {
+        fmt.Println("Error: ", err)
+    }
 }
 
 func main() {
-	fmt.Println("args: <ServerIP> <ServerPort>")
-	ServerAddr, err := net.ResolveUDPAddr("udp", os.Args[1]+":"+os.Args[2])
-	CheckError(err)
+    fmt.Println("args: <ServerIP> <ServerPort>")
+    ServerAddr, err := net.ResolveUDPAddr("udp", os.Args[1]+":"+os.Args[2])
+    CheckError(err)
 
-	LocalAddr, err := net.ResolveUDPAddr("udp", "localhost:0")
-	CheckError(err)
+    LocalAddr, err := net.ResolveUDPAddr("udp", "localhost:0")
+    CheckError(err)
 
-	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
-	CheckError(err)
+    Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+    CheckError(err)
 
-	defer Conn.Close()
-	i := 0
+    defer Conn.Close()
+    i := 0
 
-	for {		
-		/* Write to server */
-		msg := strconv.Itoa(i)
-		i++
-		buf := []byte(msg)
-		_, err := Conn.Write(buf)
-		CheckError(err)
+    for {       
+        /* Write to server */
+        msg := strconv.Itoa(i)
+        i++
+        buf := []byte(msg)
+        _, err := Conn.Write(buf)
+        CheckError(err)
 
-		/* Read from server */
-	    buf =  make([]byte, 2048)
+        /* Read from server */
+        buf =  make([]byte, 2048)
         _, err = bufio.NewReader(Conn).Read(buf)
         if err == nil {
             fmt.Printf("%s\n", buf)
@@ -46,6 +46,6 @@ func main() {
             fmt.Printf("Some error %v\n", err)
         }
 
-		time.Sleep(time.Second * 1)
-	}
+        time.Sleep(time.Second * 1)
+    }
 }
